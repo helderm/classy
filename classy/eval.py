6ae10b7f6df32e281ed23d50f3ced425951cd88b
Helder Martins <heldergaray@gmail.com>
2016-05-28 17:15:14 +0200

    Added eval for all checkpoints

--- classy/eval.py
+++ classy/eval.py
@@ -20,31 +20,28 @@ from __future__ import print_function
 
 from datetime import datetime
 import math
-import time
 import os
 
 import numpy as np
 import tensorflow as tf
 
-import classy.runway as rw
-import classy.model as cl
+import runway as rw
+import model as md
 
-NUM_EXAMPLES_PER_EPOCH_FOR_TEST = 32768 # max_images * 0.2
-NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 6553 # max_images * 0.05
-NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 98304 # total images * 0.75 training
+NUM_EXAMPLES_PER_EPOCH_FOR_TEST = 19660 # max_images * 0.2
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 13107 # max_images * 0.05
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 98305 # total images * 0.75 training
 
 FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_string('eval_dir', '../data/eval/',
                            """Directory where to write event logs.""")
-tf.app.flags.DEFINE_string('eval_type', 'test',
+tf.app.flags.DEFINE_string('eval_type', 'eval',
                            """Either 'test', 'train' or 'eval'.""")
 tf.app.flags.DEFINE_string('checkpoint_dir', '../data/train/',
                            """Directory where to read model checkpoints.""")
 tf.app.flags.DEFINE_integer('eval_interval_secs', 60 * 5,
                             """How often to run the eval.""")
-#tf.app.flags.DEFINE_integer('num_examples', NUM_EXAMPLES_PER_EPOCH_FOR_VAL,
-#                            """Number of examples to run.""")
 tf.app.flags.DEFINE_boolean('run_once', True,
                          """Whether to run eval only once.""")
 tf.app.flags.DEFINE_integer('batch_size', 16,
@@ -56,104 +53,94 @@ def _get_num_examples():
     if eval_type == 'train':
         return NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN
     elif eval_type == 'test':
-        return NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN
+        return NUM_EXAMPLES_PER_EPOCH_FOR_TEST
     elif eval_type == 'val':
         return NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
     else:
         raise Exception('Unknown eval_type')
 
-def eval_once(num_examples, saver, summary_writer, top_k_op, summary_op):
-    """Run Eval once.
-
-    Args:
-    saver: Saver.
-    summary_writer: Summary writer.
-    top_k_op: Top K op.
-    summary_op: Summary op.
-    """
-    with tf.Session() as sess:
-        ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
-        if ckpt and ckpt.model_checkpoint_path:
-            # Restores from checkpoint
-            ckpt_path = ckpt.model_checkpoint_path if not os.path.isabs(FLAGS.checkpoint_dir) \
-                            else os.path.join(FLAGS.checkpoint_dir, ckpt.model_checkpoint_path)
-            saver.restore(sess, ckpt_path)
-            # Assuming model_checkpoint_path looks something like:
-            #   /my-favorite-path/cifar10_train/model.ckpt-0,
-            # extract global_step from it.
-            global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
-        else:
-            print('No checkpoint file found')
-            return
-
-        # Start the queue runners.
-        coord = tf.train.Coordinator()
-        try:
-            threads = []
-            for qr in tf.get_collection(tf.GraphKeys.QUEUE_RUNNERS):
-                threads.extend(qr.create_threads(sess, coord=coord, daemon=True,
-                                             start=True))
-
-            num_iter = int(math.ceil(num_examples / FLAGS.batch_size))
-            true_count = 0  # Counts the number of correct predictions.
-            total_sample_count = num_iter * FLAGS.batch_size
-            step = 0
-
-            print('Evaluation started!')
-            while step < num_iter and not coord.should_stop():
-                predictions = sess.run([top_k_op])
-                correct_examples = np.sum(predictions)
-                true_count += correct_examples
-                step += 1
-
-                if step % 100 == 0:
-                    print('- {0}/{1} steps taken...'.format(step, num_iter))
-
-            # Compute precision @ 1.
-            precision = true_count / total_sample_count
-            print('%s: precision @ 1 = %.3f' % (datetime.now(), precision))
-
-            summary = tf.Summary()
-            summary.ParseFromString(sess.run(summary_op))
-            summary.value.add(tag='Precision @ 1', simple_value=precision)
-            summary_writer.add_summary(summary, global_step)
-        except Exception as e:  # pylint: disable=broad-except
-            coord.request_stop(e)
-
-    coord.request_stop()
-    coord.join(threads, stop_grace_period_secs=10)
-
 
 def evaluate():
-    """Eval CIFAR-10 for a number of steps."""
-    with tf.Graph().as_default() as g:
-        num_examples = _get_num_examples()
+    """Eval Listnr for a number of steps."""
 
-        images, labels = rw.inputs(FLAGS.batch_size, num_examples, eval_type=FLAGS.eval_type, shuffle=False)
+    ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
+    if not ckpt or not ckpt.model_checkpoint_path:
+        raise Exception('Checkpint not found!')
 
-        # Build a Graph that computes the logits predictions from the
-        # inference model.
-        logits = cl.inference(images)
+    accuracies = []
+    steps = []
+    for path in ckpt.all_model_checkpoint_paths:
 
-        # Calculate predictions.
-        top_k_op = tf.nn.in_top_k(logits, labels, 1)
+        with tf.Graph().as_default() as g:
+            # Get images and labels for CIFAR-10.
+            num_examples = _get_num_examples()
+            images, labels = rw.inputs(FLAGS.batch_size, num_examples, eval_type=FLAGS.eval_type, shuffle=False)
 
-        # Restore the moving average version of the learned variables for eval.
-        variable_averages = tf.train.ExponentialMovingAverage(
-            cl.MOVING_AVERAGE_DECAY)
-        variables_to_restore = variable_averages.variables_to_restore()
-        saver = tf.train.Saver(variables_to_restore)
+            # Build a Graph that computes the logits predictions from the
+            # inference model.
+            logits = md.inference(images)
 
-        # Build the summary operation based on the TF collection of Summaries.
-        summary_op = tf.merge_all_summaries()
+            # Calculate predictions.
+            top_k_op = tf.nn.in_top_k(logits, labels, 1)
 
-        summary_writer = tf.train.SummaryWriter(FLAGS.eval_dir, g)
+            # Restore the moving average version of the learned variables for eval.
+            variable_averages = tf.train.ExponentialMovingAverage(
+                md.MOVING_AVERAGE_DECAY)
+            variables_to_restore = variable_averages.variables_to_restore()
+            saver = tf.train.Saver(variables_to_restore)
 
-        while True:
-            eval_once(num_examples, saver, summary_writer, top_k_op, summary_op)
-            if FLAGS.run_once:
-                break
-            time.sleep(FLAGS.eval_interval_secs)
+            # Build the summary operation based on the TF collection of Summaries.
+            summary_op = tf.merge_all_summaries()
+
+            summary_writer = tf.train.SummaryWriter(FLAGS.eval_dir, g)
+
+            with tf.Session() as sess:
+
+                # Restores from checkpoint
+                ckpt_path = path if not os.path.isabs(FLAGS.checkpoint_dir) \
+                    else os.path.join(FLAGS.checkpoint_dir, path)
+                saver.restore(sess, ckpt_path)
+                # Assuming model_checkpoint_path looks something like:
+                #   /my-favorite-path/cifar10_train/model.ckpt-0,
+                # extract global_step from it.
+                global_step = ckpt_path.split('/')[-1].split('-')[-1]
+                steps.append(global_step)
+
+                # Start the queue runners.
+                coord = tf.train.Coordinator()
+                try:
+                    threads = []
+                    for qr in tf.get_collection(tf.GraphKeys.QUEUE_RUNNERS):
+                        threads.extend(qr.create_threads(sess, coord=coord, daemon=True,
+                                                         start=True))
+
+                    num_iter = int(math.ceil(num_examples / FLAGS.batch_size))
+                    true_count = 0  # Counts the number of correct predictions.
+                    step = 0
+
+                    while step < num_iter and not coord.should_stop():
+                            predictions = sess.run([top_k_op])
+                            correct_examples = np.sum(predictions)
+                            true_count += correct_examples
+                            step += 1
+
+                    # Compute precision @ 1.
+                    precision = true_count / (step * FLAGS.batch_size)
+                    print('%s: precision @ 1 = %.3f' % (datetime.now(), precision))
+                    accuracies.append(precision)
+
+                    summary = tf.Summary()
+                    summary.ParseFromString(sess.run(summary_op))
+                    summary.value.add(tag='Precision @ 1', simple_value=precision)
+                    summary_writer.add_summary(summary, global_step)
+                except Exception as e:  # pylint: disable=broad-except
+                    coord.request_stop(e)
+                finally:
+                    coord.request_stop()
+                    coord.join(threads, stop_grace_period_secs=10)
+
+    np.save(os.path.join(FLAGS.eval_dir, FLAGS.eval_type + '_accuracies'), np.array(accuracies))
+    np.save(os.path.join(FLAGS.eval_dir, FLAGS.eval_type + '_steps'), np.array(steps))
 
 
 def main(argv=None):  # pylint: disable=unused-argument
